bd4770b789b6dba9c5219f13a28b31b77449fc0b
Justine Abadicio <43513941+Justine2323@users.noreply.github.com>
2026-03-07 21:19:04 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let mybutton = document.getElementById("topBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
